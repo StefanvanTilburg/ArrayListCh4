7f7a8042f514393c11d3713972de727ee07e6096
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-09-29 10:11:43 +0200

    Opdracht 6.2 Stap 2 resultaat

--- src/controller/ArrayListLauncher.java
+++ src/controller/ArrayListLauncher.java
@@ -23,5 +23,27 @@ public class ArrayListLauncher {
         for (String woord : woorden) {
             System.out.print(woord + " ");
         }
+
+        // Stap 2: Lijst van random getallen
+        System.out.println();
+        ArrayList<Integer> randGetallenLijst = maakRandomRijGetallen(6, 10);
+        for (Integer getal : randGetallenLijst) {
+            System.out.print(getal + " ");
+        }
+    }
+
+    public static ArrayList<Integer> maakRandomRijGetallen(int aantalGetallen, int maximum) {
+        ArrayList<Integer> randomList = new ArrayList();
+
+        for (int i = 0; i < aantalGetallen; i++) {
+            randomList.add(random(maximum));
+        }
+
+        return randomList;
+    }
+
+    public static int random(int maximum) {
+        final int minimum = 1;
+        return ((int) (Math.random() * (maximum - minimum)) + minimum);
     }
 }
