89c817c42b0db46085fd5a633f464c64992282ae
StefanvanTilburg <TillyAliasStefan@gmail.com>
2020-09-29 09:00:09 +0200

    Opdracht 6.2 Stap 1 resultaat

--- src/controller/ArrayListLauncher.java
+++ src/controller/ArrayListLauncher.java
@@ -1,11 +1,27 @@
 package controller;
 
+import java.util.ArrayList;
+
 /**
  * @author Stefan van Tilburg
  * <p>
- * Opdracht
+ * Opdracht 6.2 ArrayList
  * <p>
- * Doel
+ * Doel     Oefenen met ArrayList mogelijkheden
  */
 public class ArrayListLauncher {
+    public static void main(String[] args) {
+        // Stap 1: Lijst met woorden
+        ArrayList<String> woorden = new ArrayList<>();
+        woorden.add("Morgen");
+        woorden.add("wordt");
+        woorden.add("het");
+        woorden.add("mooi");
+        woorden.add("weer");
+        woorden.remove(1);
+        woorden.add(1,"is");
+        for (String woord : woorden) {
+            System.out.print(woord + " ");
+        }
+    }
 }
